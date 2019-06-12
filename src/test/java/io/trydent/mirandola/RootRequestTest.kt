2fcbe68a038c0762f8523961ac5a1abfe492ef32
Luca Guadagnini <luca.guadagnini@cherrychain.it>
2019-06-12 13:10:56 +0200

    fixed root-request-test

--- src/test/java/io/trydent/mirandola/RootRequestTest.kt
+++ src/test/java/io/trydent/mirandola/RootRequestTest.kt
@@ -2,31 +2,39 @@ package io.trydent.mirandola
 
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
 import io.vertx.core.buffer.Buffer
+import io.vertx.core.buffer.Buffer.buffer
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.ext.web.RoutingContext
 import io.vertx.junit5.VertxExtension
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(VertxExtension::class)
 internal class RootRequestTest {
   private val context: RoutingContext = mockk()
-  private val response: HttpServerResponse = mockk()
+  private val response: FakeRootResponse = FakeRootResponse()
+  private val message = Message("Hello world.").asBuffer
 
   @Test
   internal fun `should do something`() {
     every { context.response() } returns response
-    every { response.putHeader(any(), any<String>()) } returns response
-    every { response.write(any<Buffer>()) } returns response
-    every { response.end() } returns Unit
 
-    val request = RootRequest()
+    RootRequest().handle(context)
 
-    request.handle(context)
-    verify { context.response() }
-    verify { response.putHeader("content-type", "application/json") }
-    verify { response.write(Message("Hello world.").asBuffer) }
+    assertThat(response.headers["content-type"]).isEqualTo("application/json")
+    assertThat(response.buffer).isEqualTo(message)
   }
+}
+
+private class FakeRootResponse : FakeResponse() {
+  var headers = mutableMapOf<String, String>()
+  var buffer = buffer()
+
+  override fun putHeader(name: String?, value: String?) = this.also { headers[name!!] = value!! }
+
+  override fun write(data: Buffer?): HttpServerResponse = this.also { buffer = data!! }
+
+  override fun end() = Unit
 }
