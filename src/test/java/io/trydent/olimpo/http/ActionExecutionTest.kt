721efa9a874366f884290f578865a454ad114b2e
Luca Guadagnini <luca.guadagnini@cherrychain.it>
2019-07-19 14:01:15 +0200

    removed unit test for action-execution

--- src/test/java/io/trydent/olimpo/http/ActionExecutionTest.kt
+++ src/test/java/io/trydent/olimpo/http/ActionExecutionTest.kt
@@ -3,31 +3,65 @@ package io.trydent.olimpo.http
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import io.restassured.RestAssured
+import io.restassured.http.ContentType
+import io.trydent.olimpo.apollo.Id
 import io.trydent.olimpo.dispatch.Command
 import io.trydent.olimpo.dispatch.CommandId
+import io.trydent.olimpo.io.Port
+import io.trydent.olimpo.test.json
 import io.trydent.olimpo.vertx.json
+import io.vertx.core.Vertx
 import io.vertx.core.buffer.Buffer
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.ext.web.RoutingContext
+import io.vertx.junit5.VertxExtension
+import org.hamcrest.CoreMatchers
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
-internal class ActionExecutionTest {
+@ExtendWith(VertxExtension::class)
+internal class ActionExecutionIT(vertx: Vertx) {
   private val command: Command = mockk()
-  private val context: RoutingContext = mockk()
-  private val response: HttpServerResponse = mockk()
+
+  private val httpServer = HttpServer.httpServer(
+    vertx,
+    request = HttpRequest.single(
+      vertx,
+      route = HttpRoute.action(
+        path = "/api/apollo/:action",
+        exchange = HttpExchange.actionExecution(command)
+      )
+    )
+  )
 
   @Test
-  internal fun `should handle an action and translate it to command`() {
-    every { command(any(), any()) } returns CommandId()
-    every { context.bodyAsJson } returns json()
-    every { context.response() } returns response
-    every { response.putHeader(any(), any<String>()) } returns response
-    every { response.end(any<Buffer>()) } returns Unit
+  internal fun `should execute any-action`() {
+    every {
+      command(
+        "any-action",
+        json(
+          "produced" to 3000.0,
+          "consumed" to 2000.0,
+          "putIn" to 1000.0
+        )
+      )
+    } returns Id.uuid()
 
-    val action = ActionExecution(command = command)
+    httpServer(Port.portOrDie(8090))
 
-    action().handle(context)
-
-    verify { command(any(), any()) }
+    RestAssured.given()
+      .port(8090)
+      .contentType(ContentType.JSON)
+      .json(
+        "produced" to 3000.0,
+        "consumed" to 2000.0,
+        "putIn" to 1000.0
+      )
+      .post("/api/apollo/{action}", "any-action")
+    .then()
+      .statusCode(200)
+      .contentType(ContentType.JSON)
+      .body("actionId", CoreMatchers.notNullValue())
   }
 }
