51355b84397b377331a4a0767ca2245f87df52ac
Luca Guadagnini <luca.guadagnini@cherrychain.it>
2019-07-29 14:02:24 +0200

    completed refactor in java

---
+++ src/test/java/io/trydent/olimpo/bus/CommandProcessTest.java
@@ -0,0 +1,36 @@
+package io.trydent.olimpo.bus;
+
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.json.JsonObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.trydent.olimpo.sys.Id.id;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class CommandProcessTest {
+  private interface JsonMessageConsumer extends MessageConsumer<JsonObject> {}
+
+  private final EventBus eventBus = mock(EventBus.class);
+  private final MessageConsumer<JsonObject> consumer = mock(JsonMessageConsumer.class);
+
+  @Test
+  @DisplayName("should execute a command")
+  void shouldExecuteCommand() {
+    when(eventBus.<JsonObject>localConsumer("any-command", any())).thenReturn(consumer);
+
+    final var id = id("process-id");
+    final var process = new CommandProcess(
+      id,
+      eventBus
+    );
+
+    process.accept("any-command", params -> {});
+
+    verify(eventBus).<JsonObject>localConsumer("any-command", any());
+  }
+}
